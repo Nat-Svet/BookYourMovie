33ee31a157793b341b4d55ad85f167096018b0ea
Natalia Svetlitskaya <svetlitskaya73@gmail.com>
2025-09-10 13:46:30 +0300

    ClientHeader_слова без пробелов rev.2

--- src/client/components/ClientHeader.jsx
+++ src/client/components/ClientHeader.jsx
@@ -6,10 +6,8 @@ const Header = () => {
   return (
     // Основной заголовок с классом для стилизации //
     <h1 className="header-title">
-      ИДЁМ {/* Обычная часть текста */}
-      <span className="header-light">В</span> {/* Выделенная тонким шрифтом часть текста */}
-      КИНО {/* Обычная часть текста */}
-    </h1>
+    ИДЁМ<span className="header-light">В</span>КИНО
+  </h1>
   );
 };
 
